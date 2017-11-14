fb88cee582e980eb3ed03b3246294c0895daf6cc
Sandesh <vimmrana0@gmail.com>
2017-11-14 10:21:27 +0545

    refactoring official pages

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,11 +1,11 @@
 try:
-    from urllib import quote_plus #python 2
+    from urllib import quote_plus  # python 2
 except:
     pass
 
 try:
-    from urllib.parse import quote_plus #python 3
-except: 
+    from urllib.parse import quote_plus  # python 3
+except:
     pass
 
 from django.contrib import messages
@@ -19,12 +19,12 @@ from django.utils import timezone
 
 from taggit.models import Tag
 
-
 from apps.comments.forms import CommentForm
 from apps.comments.models import Comment
 from .forms import PostForm
 from .models import Post
-from django.http import HttpResponseNotFound  
+from django.http import HttpResponseNotFound
+
 
 # def handler404(request):
 #     response = render_to_response('Http404.html', {},
@@ -37,178 +37,179 @@ from django.http import HttpResponseNotFound
 #                                   context_instance=RequestContext(request))
 #     response.status_code = 500
 #     return response
+
 # Admin stuff to workout with post manipulation
 def post_create(request):
-	if not request.user.is_staff or not request.user.is_superuser:
-		return render(request,'admin/Http404.html',status=404)
-		
+    if not request.user.is_staff or not request.user.is_superuser:
+        return render(request, 'admin/Http404.html', status=404)
 
-	form = PostForm(request.POST or None, request.FILES or None)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.user = request.user
-		instance.save()
-		# message success
-		messages.success(request, "Successfully Created")
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+        # message success
+        messages.success(request, "Successfully Created")
 
-		return HttpResponseRedirect(instance.get_absolute_url())
-	context = {
-		"form": form,
-	}
-	return render(request, "admin/create_post.html", context)
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+    return render(request, "admin/create_post.html", context)
+
 
 def post_update(request, slug=None):
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
-	instance = get_object_or_404(Post, slug=slug)
-	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
 
-		return HttpResponseRedirect(instance.get_absolute_url())
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-	context = {
-		"title": instance.title,
-		"instance": instance,
-		"form":form,
-	}
-	return render(request, "admin/create_post.html", context)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "admin/create_post.html", context)
+
 
 def post_delete(request, slug=None):
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
-	instance = get_object_or_404(Post, slug=slug)
-	instance.delete()
-	messages.success(request, "Successfully deleted")
-	return redirect("posts:list")
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
+    instance.delete()
+    messages.success(request, "Successfully deleted")
+    return redirect("posts:list")
+
 
 # users and visitors main file
 def post_list(request):
-	"""
+    """
     Displays top posts.
     """
-	today = timezone.now().date()
-	t = Tag.objects.all().order_by('-id')[:4]
+    today = timezone.now().date()
+    top_tag = Tag.objects.all().order_by('-id')[:4]
 
-	queryset_list = Post.objects.active() #.order_by("-timestamp")
-	recent_post = Post.objects.all().order_by('-id')[:3]#3 top list from descending order to time
-	recent_comment = Comment.objects.all().order_by('-id')[:3]#3 top list from descending order to time
-	# print(recent_comment);
-	if request.user.is_staff or request.user.is_superuser:
-		queryset_list = Post.objects.all()
-	query = request.GET.get("q")
-	if query:
-		queryset_list = queryset_list.filter(
-				Q(title__icontains=query)|
-				Q(content__icontains=query)|
-				Q(user__first_name__icontains=query) |
-				Q(user__last_name__icontains=query)
-				).distinct()
-	paginator = Paginator(queryset_list, 8) # Show 25 contacts per page
-	page_request_var = "page"
-	page = request.GET.get(page_request_var)
-	try:
-		queryset = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer, deliver first page.
-		queryset = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range (e.g. 9999), deliver last page of results.
-		queryset = paginator.page(paginator.num_pages)
-	context = {
-		"object_list": queryset, 
-		"title": "Top Posts",#name in the title section in list
-		"page_request_var": page_request_var,
-		"recent_post": recent_post,
-		"recent_comment":recent_comment,
-		"today": today,
-		"t":t
+    queryset_list = Post.objects.active()  # .order_by("-timestamp")
+    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    # print(recent_comment);
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        ).distinct()
+    paginator = Paginator(queryset_list, 8)  # Show 25 contacts per page
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+        "object_list": queryset,
+        "title": "Top Posts",  # name in the title section in list
+        "page_request_var": page_request_var,
+        "recent_post": recent_post,
+        "recent_comment": recent_comment,
+        "today": today,
+        "top_tag": top_tag
 
-	}
-	#authentication
-	if not request.user.is_authenticated():
-		return render(request, "user/post_list.html", context)
-	else:
-		return render(request, "user/post_list.html", context)
+    }
+    # authentication
+    if not request.user.is_authenticated():
+        return render(request, "user/post_list.html", context)
+    else:
+        return render(request, "user/post_list.html", context)
+
 
 def post_detail(request, slug=None):
-	"""
+    """
     Displays details of Post.
     """
-    #taggit
-	# tag = Post.objects.filter(
-	#     tags__name__in=list(self.objects.tags.values_list('name', flat=True))
-	# ).exclude(id=self.object.id)
-	# tag = Post..filter(pk=pk)
-	# tag_names = [tag.name for tag in Tag.objects.all()]
-	# print(tag_names)
-	# import pdb; pdb.set_trace()
-	instance = get_object_or_404(Post, slug=slug)
-	recent_post = Post.objects.all().order_by('-id')[:3]#3 top list from descending order to time
-	recent_comment = Comment.objects.all().order_by('-id')[:3]#3 top list from descending order to time
-	t = Tag.objects.all().order_by('-id')[:4]#for sidbar top tag
-	# print(t)
-	tag = instance.tags.all()
-	
+    # taggit
+    # tag = Post.objects.filter(
+    #     tags__name__in=list(self.objects.tags.values_list('name', flat=True))
+    # ).exclude(id=self.object.id)
+    # tag = Post..filter(pk=pk)
+    # tag_names = [tag.name for tag in Tag.objects.all()]
+    # print(tag_names)
+    # import pdb; pdb.set_trace()
+    instance = get_object_or_404(Post, slug=slug)
+    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    top_tag = Tag.objects.all().order_by('-id')[:4]  # for sidbar top tag
+    # print(t)
+    tag = instance.tags.all()
 
-	num_tags = tag.count()
-	print(tag.count())
-	# tag = instance
-	if instance.publish > timezone.now().date() or instance.draft:
-		if not request.user.is_staff or not request.user.is_superuser:
-			raise Http404
-	share_string = quote_plus(instance.content)
+    num_tags = tag.count()
+    print(tag.count())
+    # tag = instance
+    if instance.publish > timezone.now().date() or instance.draft:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+    share_string = quote_plus(instance.content)
 
-	initial_data = {
-			"content_type": instance.get_content_type,
-			"object_id": instance.id
-	}
-	form = CommentForm(request.POST or None, initial=initial_data)
-	if form.is_valid() and request.user.is_authenticated():
-		c_type = form.cleaned_data.get("content_type")
-		content_type = ContentType.objects.get(model=c_type)
-		obj_id = form.cleaned_data.get('object_id')
-		content_data = form.cleaned_data.get("content")
-		parent_obj = None
-		try:
-			parent_id = int(request.POST.get("parent_id"))
-		except:
-			parent_id = None
+    initial_data = {
+        "content_type": instance.get_content_type,
+        "object_id": instance.id
+    }
+    form = CommentForm(request.POST or None, initial=initial_data)
+    if form.is_valid() and request.user.is_authenticated():
+        c_type = form.cleaned_data.get("content_type")
+        content_type = ContentType.objects.get(model=c_type)
+        obj_id = form.cleaned_data.get('object_id')
+        content_data = form.cleaned_data.get("content")
+        parent_obj = None
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except:
+            parent_id = None
 
-		if parent_id:
-			parent_qs = Comment.objects.filter(id=parent_id)
-			if parent_qs.exists() and parent_qs.count() == 1:
-				parent_obj = parent_qs.first()
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if parent_qs.exists() and parent_qs.count() == 1:
+                parent_obj = parent_qs.first()
 
+        new_comment, created = Comment.objects.get_or_create(
+            user=request.user,
+            content_type=content_type,
+            object_id=obj_id,
+            content=content_data,
+            parent=parent_obj,
+        )
+        return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
 
-		new_comment, created = Comment.objects.get_or_create(
-							user = request.user,
-							content_type= content_type,
-							object_id = obj_id,
-							content = content_data,
-							parent = parent_obj,
-						)
-		return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+    comments = instance.comments
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "share_string": share_string,
+        "comments": comments,
+        "comment_form": form,
+        "tag": tag,
+        "num_tags": num_tags,
+        "recent_post": recent_post,
+        "recent_comment": recent_comment,
+        "top_tag": top_tag
 
-
-	comments = instance.comments
-	context = {
-		"title": instance.title,
-		"instance": instance,
-		"share_string": share_string,
-		"comments": comments,
-		"comment_form":form,
-		"tag":tag,
-		"num_tags":num_tags,
-		"recent_post":recent_post,
-		"recent_comment":recent_comment,
-		"t":t
-
-	}
-	#authentication
-	if not request.user.is_authenticated():
-		return render(request, "user/post_detail.html", context)
-	else:
-		return render(request, "user/post_detail.html", context)
+    }
+    # authentication
+    if not request.user.is_authenticated():
+        return render(request, "user/post_detail.html", context)
+    else:
+        return render(request, "user/post_detail.html", context)
--- apps/web_sikhshalaya/views.py
+++ apps/web_sikhshalaya/views.py
@@ -1,32 +1,63 @@
 from django.shortcuts import render, get_object_or_404
+
 from apps.posts.models import *
+from taggit.models import Tag
+
+
 # Create your views here.
 # def about_office_view(request):
-	# instance = get_object_or_404(Post, slug=slug)
-	# return render(request, "office_policy/about_office.html" )
+# instance = get_object_or_404(Post, slug=slug)
+# return render(request, "office_policy/about_office.html" )
 
 def about_view(request):
-	# instance = get_object_or_404(Post, slug=slug)
-	return render(request, "office_policy/about.html" )
+    # instance = get_object_or_404(Post, slug=slug)
+    return render(request, "office_policy/about.html")
+
 
 def policy_view(request):
-	return render(request, "office_policy/policy.html")
+    return render(request, "office_policy/policy.html")
+
 
 def terms_view(request):
-	return render(request, "office_policy/terms.html")
+    return render(request, "office_policy/terms.html")
+
 
 def sitemap_view(request):
-	instance = Post.objects.all()
-	context = {
-	"instance":instance,
-	}
-	return render(request, "official/sitemap.html", context)
+    instance = Post.objects.all()
+    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    top_tag = Tag.objects.all().order_by('-id')[:4]  # for sidbar top tag
+
+    context = {
+        "recent_post": recent_post,
+        "recent_comment": recent_comment,
+        "instance": instance,
+        "top_tag": top_tag,
+    }
+    return render(request, "official/sitemap.html", context)
+
 
 def resource_view(request):
-	# app_url = request.path
-	return render(request, "official/resource.html")
+    # app_url = request.path
+    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    top_tag = Tag.objects.all().order_by('-id')[:4]  # for sidbar top tag
+    context = {
+        "recent_post": recent_post,
+        "recent_comment": recent_comment,
+        "top_tag": top_tag,
+    }
+    return render(request, "official/resource.html", context)
+
 
 def contact_view(request):
-	# app_url = request.path
-	return render(request, "official/contact.html")
-
+    # app_url = request.path
+    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
+    top_tag = Tag.objects.all().order_by('-id')[:4]  # for sidbar top tag
+    context = {
+        "recent_post": recent_post,
+        "recent_comment": recent_comment,
+        "top_tag": top_tag,
+    }
+    return render(request, "official/contact.html", context)
