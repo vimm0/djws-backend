a117869c2d8f4200633de6f542b7c6f524bca2f0
Sandesh <vimmrana0@gmail.com>
2017-11-17 17:38:57 +0545

    web_sikhshalaya app to CBV

--- apps/web_sikhshalaya/views.py
+++ apps/web_sikhshalaya/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from taggit.models import Tag
 
@@ -6,60 +6,40 @@ from apps.posts.models import Post
 from apps.comments.models import Comment
 
 
-
-# Create your views here.
-# def about_office_view(request):
-# instance = get_object_or_404(Post, slug=slug)
-# return render(request, "office_policy/about_office.html" )
-
-def about_view(request):
-    # instance = get_object_or_404(Post, slug=slug)
-    return render(request, "office_policy/about.html")
+class AboutList(ListView):
+    model = Post
+    template_name = "office_policy/about.html"
 
 
-def policy_view(request):
-    return render(request, "office_policy/policy.html")
+class PolicyList(ListView):
+    model = Post
+    template_name = "office_policy/policy.html"
 
 
-def terms_view(request):
-    return render(request, "office_policy/terms.html")
+class TermList(ListView):
+    model = Post
+    template_name = "office_policy/terms.html"
 
 
-def sitemap_view(request):
-    instance = Post.objects.all()
-    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
-    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
-    top_tag = Tag.objects.all().order_by('-id')[:4]  # for sidbar top tag
-    context = {
-        "recent_post": recent_post,
-        "recent_comment": recent_comment,
-        "instance": instance,
-        "top_tag": top_tag,
-    }
-    return render(request, "official/sitemap.html", context)
+class SitemapList(ListView):
+    model = Post
+    queryset = Post.objects.all()
+    template_name = "official/sitemap.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SitemapList, self).get_context_data(**kwargs)
+        context['recent_comment'] = Comment.objects.all().order_by('-id')[:3]
+        context['top_tag'] = Tag.objects.all().order_by('-id')[:4]
+        return context
 
 
-def resource_view(request):
-    # app_url = request.path
-    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
-    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
-    top_tag = Tag.objects.all().order_by('-id')[:4]  # for sidbar top tag
-    context = {
-        "recent_post": recent_post,
-        "recent_comment": recent_comment,
-        "top_tag": top_tag,
-    }
-    return render(request, "official/resource.html", context)
+class ResourceList(ListView):
+    model = Post
+    queryset = Post.objects.all().order_by('-id')[:3]
+    template_name = "official/resource.html"
 
 
-def contact_view(request):
-    # app_url = request.path
-    recent_post = Post.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
-    recent_comment = Comment.objects.all().order_by('-id')[:3]  # 3 top list from descending order to time
-    top_tag = Tag.objects.all().order_by('-id')[:4]  # for sidbar top tag
-    context = {
-        "recent_post": recent_post,
-        "recent_comment": recent_comment,
-        "top_tag": top_tag,
-    }
-    return render(request, "official/contact.html", context)
+class ContactList(ListView):
+    model = Post
+    queryset = Post.objects.all().order_by('-id')[:3]
+    template_name = "official/contact.html"
