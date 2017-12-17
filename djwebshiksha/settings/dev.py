1fec0d6c67d060eac854bb2ef198b8fcb0f4a061
Sandesh <vimmrana0@gmail.com>
2017-12-17 21:00:22 +0545

    Adds changed environment

--- djwebshiksha/settings/dev.py
+++ djwebshiksha/settings/dev.py
@@ -3,14 +3,17 @@ from .base import *
 SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 
 DEBUG = True
+print(BASE_DIR)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '..', 'static'),
+)
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "..", "static")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "..", "media")
 
 INSTALLED_APPS += (
     'debug_toolbar',
