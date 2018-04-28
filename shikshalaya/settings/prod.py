832857e85dcddf59df5f9f5da4c075b1cf6e513b
vimm0 <vimmrana0@gmail.com>
2018-04-28 11:41:27 +0545

    stable config

--- shikshalaya/settings/prod.py
+++ shikshalaya/settings/prod.py
@@ -3,6 +3,10 @@ from dj_database_url import config
 
 from .base import *
 
+"""
+    DEBUG = os.environ.get('DEBUG', True) finds the DEBUG config from heroku vars, if not found fallback to TRUE
+"""
+######### STABLE #########
 DEBUG = os.environ.get('DEBUG', True)
 # SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 SECRET_KEY = os.environ.get('SECRET_KEY', 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x')
